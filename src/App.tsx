c1418f619f820d7f3a296a3073efc3c8240e3f79
Florian Martin <56591109+NeoFeear@users.noreply.github.com>
2022-02-17 17:16:22 +0100

    Add Rows + Add Banner

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,25 @@
 /* eslint-disable import/no-anonymous-default-export */
 import React from 'react';
-import API_Popular from './components/API_Popular';
+
 import Navbar from './components/Navbar';
+import Banner from './components/Banner';
+import Row from './components/Row';
+import requests from './components/Requests';
 
 function App() {
     return (
-        <>
-            <API_Popular />
-            <Navbar />
-        </>
+        <div className="app">
+            {/* <Navbar /> */}
+
+            <Banner />
+
+            <Row title="Trending Now" fetchUrl={requests.trending} />
+            <Row title="Netflix Originals" fetchUrl={requests.netflixOriginals} />
+            <Row title="Top Rated" fetchUrl={requests.topRated} />
+            <Row title="Action Movies" fetchUrl={requests.actionMovies} />
+            <Row title="Comedy Movies" fetchUrl={requests.comedyMovies} />
+            <Row title="Horror Movies" fetchUrl={requests.horrorMovies} />
+        </div>
     );
 }
 
---
+++ src/components/Row.tsx
@@ -0,0 +1,44 @@
+import React, { useEffect, useState } from "react";
+import axios from "./Axios";
+import "./Row.css";
+
+interface RowProps {
+    title: string;
+    fetchUrl: string;
+}
+
+function Row({ title, fetchUrl }: RowProps) {
+    const [movies, setMovies] = useState([]);
+
+    useEffect (() => {
+
+        async function fetchData() {
+            const request = await axios.get(fetchUrl);
+            setMovies(request.data.results);
+            return request;
+        }
+
+        fetchData();
+
+    }, [fetchUrl]);
+
+    console.log(title, movies);
+
+    return(
+        <div className="row">
+            <h2>{title}</h2>
+
+            <div className="row__posters">
+                {movies.map((movie: any) => (
+                    <img className="row__poster"
+                         key={movie.id}
+                         src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`}
+                         alt={movie?.title || movie?.original_title || movie?.name || movie?.original_name}
+                    />
+                ))}
+            </div>
+        </div>
+    );
+}
+
+export default Row;
